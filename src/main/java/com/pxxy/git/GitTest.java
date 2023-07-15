6f74412da1e09e4d8eb53e7da802ca9c7af28952
zhang <zhang@gmail.com>
2023-07-15 17:03:10 +0800

    first commit

---
+++ src/main/java/com/pxxy/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.pxxy.git;
+
+/**
+ * Class Name:GitTest
+ * Description:
+ * Author:张剑剑
+ * Create:2023 - 07 - 2023/7/15 - 16:58
+ *
+ * @Version: v1.8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
