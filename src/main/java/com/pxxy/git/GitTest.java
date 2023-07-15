60cf4340cb0f17b24e52e7dbf5c3412b318003f4
zhang <zhang@gmail.com>
2023-07-15 17:12:51 +0800

    second commit

--- src/main/java/com/pxxy/git/GitTest.java
+++ src/main/java/com/pxxy/git/GitTest.java
@@ -11,5 +11,7 @@ package com.pxxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
+
 }
